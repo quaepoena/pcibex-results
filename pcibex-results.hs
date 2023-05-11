f2cd5bc98f55a689f31b9724c80de9262839a1fe
QP <qp@example.com>
2023-05-11 14:46:16 +0200

    removing the Result data type, as it seemed like overkill

--- pcibex-results.hs
+++ pcibex-results.hs
@@ -4,19 +4,17 @@ import System.IO(readFile)
 
 import qualified Data.Map.Strict as Map
 
--- User ID, item number, value given.
-data Result = Result String Int Int
-
 relevantLines :: [String] -> Bool
 relevantLines result = length result == 13 &&
                        result !! 5 == "experimental-trial" &&
                        result !! 7 == "Scale" &&
                        result !! 10 /= "NA"
 
-toResult :: [String] -> Result
-toResult line = Result (line !! 0 ++ "-" ++ line !! 1)
-                (read (line !! 3) :: Int)
-                (read (line !! 10) :: Int)
+-- Extract the user ID, item number, and value given.
+toResult :: [String] -> (String, Int, Int)
+toResult line = (line !! 0 ++ "-" ++ line !! 1,
+                 read (line !! 3) :: Int,
+                 read (line !! 10) :: Int)
 
 perRating :: (String, [(Int, Int)]) -> [(String, Int, Int)]
 perRating (_, []) = []
@@ -39,7 +37,7 @@ main = do
       -- Remove users that didn't complete the experiment.
       resultsByUser = Map.filter (\ x -> length x == 24)
                     . Map.fromListWith (++)
-                    $ map (\ (Result user item value) -> (user, [(item, value)])) results
+                    $ map (\ (user, item, value) -> (user, [(item, value)])) results
 
       validResults = foldl1 (++) . map perRating $ Map.toList resultsByUser
 
