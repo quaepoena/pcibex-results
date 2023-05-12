17a74382f798cd7a06fca078e7733cd3464cf14d
QP <qp@example.com>
2023-05-12 13:42:15 +0200

    specifying the target and control items

--- pcibex.r
+++ pcibex.r
@@ -13,20 +13,21 @@ items <- c(3,5,7,9,11,13)
 #items <- c(4,6,8,10,12,14)
 
 data <- csv_data %>% filter(item %in% items)
+
+data <- aggregate(data$value, by = list(data$item),
                   FUN = function(x) c(mean = mean(x), sd = sd(x), n = length(x)))
 data <- do.call(data.frame, data)
 data$se <- data$x.sd / sqrt(data$x.n)
 colnames(data) <- c("item", "mean", "sd", "n", "se")
-data$names <- c("/01/", "/02/", "/03/", "/04/", "/05/", "/06/", "/07/", "/08/",
-                "/09/", "/10/", "/11/", "/12/", "/13/", "/14/", "/15/", "/16/",
-                "/17/", "/18/", "/19/", "/20/", "/21/", "/22/", "/23/", "/24/")
+
+data$items <- c("/01/", "/03/", "/05/", "/07/", "/09/", "/11/")
+#data$items <- c("/02/", "/04/", "/06/", "/08/", "/10/", "/12/")
 
 dodge <- position_dodge(width = 0.9)
 limits <- aes(ymax = mean + se, ymin = mean - se)
 
-p <- ggplot(data = data, aes(x = names, y = mean, fill = names))
+p <- ggplot(data = data, aes(x = items, y = mean, fill = items))
 
 p + geom_bar(stat = "identity", position = dodge) +
     geom_errorbar(limits, position = dodge, width = 0.25) +
-    theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(),
-          axis.title.x=element_blank())
+    theme(axis.ticks.x=element_blank())
